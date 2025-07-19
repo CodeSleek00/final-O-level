4dac397164d19fd6617d2da0d32e1650db97b8a9
CodeSleek00 <codesleekstudio@gmail.com>
2025-07-19 10:25:43 +0530

    main

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include 'navbar.html'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>faiz</h1>
+</body>
+</html>
