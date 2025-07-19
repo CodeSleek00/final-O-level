ee7826cdb412dc970d7dadad17c3c4c060816020
CodeSleek00 <codesleekstudio@gmail.com>
2025-07-19 23:08:05 +0530

    basic

--- index.php
+++ index.php
@@ -4,11 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Favicon for browsers -->
-    <link rel="icon" type="image/ico" href="o level.ico">
-    <!-- Apple Touch Icon should ideally be PNG (not .ico) -->
-    <link rel="apple-touch-icon" href="olevel.png">
-
+    <link rel="icon" type="image/ico" href="images/o level.ico">
+    <link rel="apple-touch-icon" href="images/olevel.png">
     <title>O level Practice</title>
 </head>
 <body>
