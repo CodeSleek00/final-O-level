51038c62262813b250cf72ee4c677c759539a107
CodeSleek00 <codesleekstudio@gmail.com>
2025-07-19 11:10:50 +0530

    change

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="icon" type="images/png" href="o level.png">
+    <link rel="apple-touch-icon" href="images/o level.png">
+    <title>O level Practice </title>
 </head>
 <body>
     <h1>faiz</h1>
