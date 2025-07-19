18c69b5d3b61d91c6e2f9f3bab7e801132fd3c4e
CodeSleek00 <codesleekstudio@gmail.com>
2025-07-19 23:17:01 +0530

    change

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/ico" href="images/o level.ico">
     <link rel="apple-touch-icon" href="images/olevel.png">
     <title>O level Practice</title>
