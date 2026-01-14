d5f65bf4854ba4ed8b9004f78bf017bc451933c9
CodeSleek00 <codesleekstudio@gmail.com>
2026-01-14 18:29:40 +0530

    Update M1-R5.php

--- olevel/M1-R5.php
+++ olevel/M1-R5.php
@@ -7,11 +7,12 @@ $subject_id = 1; // IT Tools (M1-R5)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <title>M1-R5 (IT Tools)</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
 
-    <title>M1-R5 (IT Tools)</title>
 
     <style>
         body{
