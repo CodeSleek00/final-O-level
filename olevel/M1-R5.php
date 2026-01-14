7344af5bfe6a3bc8c5933ea3004eeee37cdfe80e
CodeSleek00 <codesleekstudio@gmail.com>
2026-01-14 18:43:10 +0530

    Update M1-R5.php

--- olevel/M1-R5.php
+++ olevel/M1-R5.php
@@ -81,7 +81,7 @@ $subject_id = 1; // IT Tools (M1-R5)
             border-radius: 18px;
             box-shadow: 0 10px 30px rgba(0,0,0,0.06);
             text-align: center;
-            margin-bottom: auto;
+            margin-bottom: 50px;
             background-color: black;
             color: white;
         }
@@ -182,6 +182,7 @@ $subject_id = 1; // IT Tools (M1-R5)
         @media (max-width: 480px) {
             .it-banner {
                 padding: 40px 25px;
+                margin-bottom: 0px;
             }
 
             .it-banner h1 {
