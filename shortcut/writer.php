c9893773dffa1844bd3353017b863ed1476a6de3
CodeSleek00 <codesleekstudio@gmail.com>
2026-01-15 19:54:15 +0530

    Update writer.php

--- shortcut/writer.php
+++ shortcut/writer.php
@@ -11,7 +11,6 @@ $result = $conn->query("SELECT * FROM shortcuts WHERE category='LibreOffice Writ
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
 
 <?php include 'navbar.html'; ?>
 
