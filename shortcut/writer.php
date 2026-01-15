4e6464c1c0a1a77975725b811554db5b71dade2d
CodeSleek00 <codesleekstudio@gmail.com>
2026-01-15 19:53:44 +0530

    Update writer.php

--- shortcut/writer.php
+++ shortcut/writer.php
@@ -2,6 +2,16 @@
 include("../db_connect.php");
 $result = $conn->query("SELECT * FROM shortcuts WHERE category='LibreOffice Writer'");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LibreOffice Writer Shortcut Keys | Faiz Computer Institute</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
 
 <?php include 'navbar.html'; ?>
 
