1648bf98fc87e5534eea69cbf993a7b3aae28947
CodeSleek00 <codesleekstudio@gmail.com>
2026-01-18 20:41:36 +0530

    xc

--- exam/chapter_exam.php
+++ exam/chapter_exam.php
@@ -218,7 +218,7 @@ h2{
 
 <!-- LEFT BANNER -->
 <div class="side-banner left-banner">
-    <img src="c.png" alt="Left Banner">
+    <img src="faiz-left.png" alt="Left Banner">
 </div>
 
 <!-- RIGHT BANNER -->
